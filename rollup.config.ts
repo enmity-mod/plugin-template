fcecd458ddb415f6f9cc091e6a9718221f0b6c72
eternal <eternal.git@gmail.com>
2022-05-29 09:49:42 +0100

    update plugin template

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,43 +1,43 @@
-import { defineConfig, Plugin } from "rollup";
-import esbuild from "rollup-plugin-esbuild";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig, Plugin } from 'rollup';
+import esbuild from 'rollup-plugin-esbuild';
 
-import { basename } from "path";
-import { writeFileSync } from "fs";
+import { basename } from 'path';
+import { writeFileSync } from 'fs';
 
 const pluginName = basename(process.cwd());
 
 export default defineConfig({
-  input: "src/index.ts",
-  output: [
-    {
-      file: `dist/${pluginName}.js`,
-      format: "cjs",
-      strict: false
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    esbuild({ minify: true, target: "ES2019" }),
-    createPluginJson(),
-  ]
+   input: 'src/index.ts',
+   output: [
+      {
+         file: `dist/${pluginName}.js`,
+         format: 'cjs',
+         strict: false
+      },
+   ],
+   plugins: [
+      nodeResolve(),
+      commonjs(),
+      esbuild({ minify: true, target: 'ES2019' }),
+      createPluginJson(),
+   ]
 });
 
-function createPluginJson(options = {}): Plugin {
-  return {
-    name: 'plugin-info',
-    writeBundle: (err) => {
-      const info = require('./package.json');
-      const data = {
-        "name": pluginName,
-        "description": info?.description ?? "No description was provided.",
-        "author": info?.author?.name ?? "Unknown",
-        "version": info?.version ?? "1.0.0"
-      };
+function createPluginJson(): Plugin {
+   return {
+      name: 'plugin-info',
+      writeBundle: () => {
+         const info = require('./package.json');
+         const data = {
+            'name': pluginName,
+            'description': info?.description ?? 'No description was provided.',
+            'author': info?.author?.name ?? 'Unknown',
+            'version': info?.version ?? '1.0.0'
+         };
 
-      writeFileSync(`dist/${pluginName}.json`, JSON.stringify(data, null, "\t"));
-    }
-  }
+         writeFileSync(`dist/${pluginName}.json`, JSON.stringify(data, null, '\t'));
+      }
+   };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-import { Plugin, registerPlugin } from "enmity-api/plugins";
+import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { getByProps } from 'enmity/metro';
+import { create } from 'enmity/patcher';
 
-const ExamplePlugin: Plugin = {
-  name: "ExamplePlugin",
-  commands: [],
-  patches: [],
+const Typing = getByProps('startTyping');
 
-  onStart() {
+const Patcher = create('silent-typing');
 
-  },
+const SilentTyping: Plugin = {
+   name: 'SilentTyping',
+   authors: [
+      {
+         name: 'eternal',
+         id: '263689920210534400'
+      }
+   ],
 
-  onStop() {
+   onStart() {
+      Patcher.instead(Typing, 'startTyping', () => { });
+      Patcher.instead(Typing, 'stopTyping', () => { });
+   },
 
-  }
-}
+   onStop() {
+      Patcher.unpatchAll();
+   }
+};
 
-registerPlugin(ExamplePlugin);
+registerPlugin(SilentTyping);
