2e897a9862282baec8f7d4d414ae1d089d763fee
eternal <eternal.git@gmail.com>
2022-07-07 21:08:28 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { React } from 'enmity/metro/common';
 import { getByProps } from 'enmity/metro';
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
